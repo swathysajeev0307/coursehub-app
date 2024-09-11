5c7b42e208b7fcbe17870eb7cd67609cf1985e3e
swathysajeev0307 <swathysajeev35@gmail.com>
2024-09-11 13:57:15 +0530

    Added files directly from file

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO COURSEHUB' title='Best Online Learning Platform' />
+            <p>"CourseHub..Where Learning Meets Excellence..Empowering Minds, Shaping Futures..Your Journey to Knowledge Begins Here.."</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
